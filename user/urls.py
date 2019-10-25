b74abee5fc095b583d644d005d429d74a261887c
BANDAHN NAMBOONMA <iryudragon01@gmail.com>
2019-10-25 20:17:30 +0700

    login part2

--- user/urls.py
+++ user/urls.py
@@ -5,7 +5,8 @@ from . import  views
 app_name = 'user'
 
 urlpatterns = [
-    path('',views.IndexView,name='index'),
+
+    path('', views.LoginView, name='index'),
     path('register/',views.RegisterView,name='register'),
-    path('login/',views.LoginView,name='login'),
+    path('logout',views.LogoutView,name='logout')
 ]
