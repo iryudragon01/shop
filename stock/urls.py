0853b3fe1c450b4741b22d3161ecde7631fdce57
BANDAHN NAMBOONMA <iryudragon01@gmail.com>
2019-10-27 23:32:02 +0700

    navbar layout

--- stock/urls.py
+++ stock/urls.py
@@ -3,17 +3,7 @@ from . import views
 app_name='stock'
 urlpatterns=[
     path('',views.IndexView,name='index'),
-    path('test/',views.DisplayView,name='test'),
     path('topup/',views.Add_top_up,name='topup'),
-    path('topup/<int:pk>/',views.DetailtopupView.as_view(),name='detail'),
-
-
-    ##### Item management
     path('itemcreate/',views.ItemCreateView,name='itemcreate'),
-    path('itemlist/',views.ItemListView,name='itemlist'),
-    path('itemedit/<int:pk>/',views.ItemEditView,name='itemedit'),
-    ####  topup management
-    path('topupcreate/',views.TopupCreateView,name='topupcreate'),
-    path('topuplist/',views.TopupListView,name='topuplist')
 
 ]
--- stock/views.py
+++ stock/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
-from django.views import generic
-from .models import Item, Top_up, Log_Sheet, Display_Item
+from .models import Item
 from .iryu.ItemManage import Manage
 from .iryu.Top_up_link import Top_up_work
 
@@ -24,11 +23,6 @@ def IndexView(request):
                 content = Display.getdisplay(Display, request)
                 return render(request, 'stock/index.html', content)
 
-
-def DisplayView(request):
-    return render(request, 'stock/testhtml.html', {})
-
-
 #########Item management
 def ItemCreateView(request):
     content = {}
@@ -39,34 +33,25 @@ def ItemCreateView(request):
     return render(request, 'stock/Item/create.html', content)
 
 
-def ItemListView(request):
-    content = Manage.getitemlist(Manage)
-    return render(request, 'stock/Item/list.html', content)
-
 
 def ItemEditView(request, pk=None):
     if pk is not None:
         content = Manage.loaditem(Manage, pk)
         if request.method == 'POST':
             Manage.saveedititem(Manage, pk, request)
-            return ItemListView(request)
+            return redirect('stock:index')
         return render(request, 'stock/Item/Edit.html', content)
 
 
 ########end Item page
 ######## topup page
 
-def TopupCreateView(request):
+def Top_up_Create_View(request):
     content = Top_up_work.getfooditem(Top_up_work)
     if request.method == 'POST':
         Top_up_work.create(Top_up_work, request)
-        return TopupListView(request)
+        return redirect('stock:index')
     return render(request, 'stock/topup/create.html', content)
-
-
-def TopupListView(request):
-    content = Top_up_work.list(Top_up_work)
-    return render(request, 'stock/topup/list.html', content)
 
 def Add_top_up(request):
     items=Item.objects.filter(type=3)
@@ -76,26 +61,3 @@ def Add_top_up(request):
 
     return render(request,'stock/topup/top_up.html',{'items':items})
 
-
-# view by class
-# class IndexView(generic.ListView):
-#     template_name = 'stock/index.html'
-#     context_object_name = 'latest_all_list'
-
-#     def get_queryset(self):
-#         allitem=Item.objects.all()
-#         lastdict={}
-#         randomval=9
-#         for id in allitem:
-#             print(id.id)
-#             lastdict[id]=randomval
-#             randomval+=7
-
-#         Model2List.Logsheet(Model2List,version=2,logdictionary=lastdict)
-#         return Model2List.ListItem(Model2List)
-
-
-
-class DetailtopupView(generic.DetailView):
-    model = Top_up
-    template_name = 'stock/detail.html'
